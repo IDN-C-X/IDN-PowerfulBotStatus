179fb577d6459413b030dc0887ff760326e91475
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-19 20:55:01 +0700

    First Commit `IDN-C-X`

--- BotStatus.py
+++ BotStatus.py
@@ -21,40 +21,40 @@ CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
 MESSAGE_ID = int(os.environ["MESSAGE_ID"])
 BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
 
-async def main_teletips():
+async def main_idncoder():
     async with app:
             while True:
                 print("Checking The System...")
                 GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
                 CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
                 CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
-                xxx_teletips = f"📊 **<u>LIVE BOT STATUS</u>**\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
+                xxx_idncoder = f"📊 **<u>LIVE BOT STATUS</u>**\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
                 for bot in BOT_LIST:
                     try:
-                        yyy_teletips = await app.send_message(bot, "/start")
-                        aaa = yyy_teletips.message_id
-                        await asyncio.sleep(10)
-                        zzz_teletips = await app.get_history(bot, limit = 1)
-                        for ccc in zzz_teletips:
+                        yyy_idncoder = await app.send_message(bot, "/start")
+                        aaa = yyy_idncoder.message_id
+                        await asyncio.sleep(7)
+                        zzz_idncoder = await app.get_history(bot, limit = 1)
+                        for ccc in zzz_idncoder:
                             bbb = ccc.message_id
                         if aaa == bbb:
-                            xxx_teletips += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: down ❌"
+                            xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: Down ❌"
                             for bot_admin_id in BOT_ADMIN_IDS:
                                 try:
-                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep!! @{bot} is down** ❌")
+                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep! @{bot} is down** ❌")
                                 except Exception:
                                     pass
                             await app.read_history(bot)
                         else:
-                            xxx_teletips += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: alive ✅"
+                            xxx_teletips += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: Alive ✅"
                             await app.read_history(bot)
                     except FloodWait as e:
                         await asyncio.sleep(e.x)            
                 time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
                 last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-                xxx_teletips += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every 45 min - Powered By @IDNCoderX</i>"
+                xxx_idncoder += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every 45 minutes - Powered By @IDNCoderX</i>"
                 await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
                 print(f"Last checked on: {last_update}")                
-                await asyncio.sleep(2700)
+                await asyncio.sleep(1500)
                         
-app.run(main_teletips())
+app.run(main_idncoder())

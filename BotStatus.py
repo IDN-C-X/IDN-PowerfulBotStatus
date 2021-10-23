4bb3a20fc2cc68703343509e63abfad7901330bf
Sourcery AI <>
2021-10-23 09:48:10 +0000

    'Refactored by Sourcery'

--- BotStatus.py
+++ BotStatus.py
@@ -24,38 +24,37 @@ COOLDOWN = int(os.environ["COOLDOWN"])
 
 async def main_idncoder():
     async with app:
-            while True:
-                print("Checking The System...")
-                GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
-                CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
-                CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
-                xxx_idncoder = f"📊 **<u>LIVE BOT STATUS</u>**\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
-                for bot in BOT_LIST:
-                    try:
-                        yyy_idncoder = await app.send_message(bot, "/start")
-                        aaa = yyy_idncoder.message_id
-                        await asyncio.sleep(7)
-                        zzz_idncoder = await app.get_history(bot, limit = 1)
-                        for ccc in zzz_idncoder:
-                            bbb = ccc.message_id
-                        if aaa == bbb:
-                            xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: Down ❌"
-                            for bot_admin_id in BOT_ADMIN_IDS:
-                                try:
-                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep! @{bot} is down** ❌")
-                                except Exception:
-                                    pass
-                            await app.read_history(bot)
-                        else:
-                            xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: Alive ✅"
-                            await app.read_history(bot)
-                    except FloodWait as e:
-                        await asyncio.sleep(e.x)            
-                time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
-                last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-                xxx_idncoder += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every {COOLDOWN} sec\nPowered By @IDNCoderX</i>"
-                await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
-                print(f"Last checked on: {last_update}")                
-                await asyncio.sleep(COOLDOWN)
+        while True:
+            print("Checking The System...")
+            GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
+            CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
+            CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
+            xxx_idncoder = f"📊 **<u>LIVE BOT STATUS</u>**\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
+            for bot in BOT_LIST:
+                try:
+                    yyy_idncoder = await app.send_message(bot, "/start")
+                    aaa = yyy_idncoder.message_id
+                    await asyncio.sleep(7)
+                    zzz_idncoder = await app.get_history(bot, limit = 1)
+                    for ccc in zzz_idncoder:
+                        bbb = ccc.message_id
+                    if aaa == bbb:
+                        xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: Down ❌"
+                        for bot_admin_id in BOT_ADMIN_IDS:
+                            try:
+                                await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep! @{bot} is down** ❌")
+                            except Exception:
+                                pass
+                    else:
+                        xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: Alive ✅"
+                    await app.read_history(bot)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+            last_update = time.strftime('%d %b %Y at %I:%M %p')
+            xxx_idncoder += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every {COOLDOWN} sec\nPowered By @IDNCoderX</i>"
+            await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
+            print(f"Last checked on: {last_update}")
+            await asyncio.sleep(COOLDOWN)
                         
 app.run(main_idncoder())
